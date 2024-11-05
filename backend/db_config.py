712e937f18c35052ca8509f362d1a16878009365
PON-AJITH-KUMAR-PONRAJ <ponajithkumar05@gmail.com>
2024-11-05 22:54:32 +0530

    Updated the code

--- backend/db_config.py
+++ backend/db_config.py
@@ -1,9 +1,12 @@
+from flask_sqlalchemy import SQLAlchemy
 import mysql.connector
+
+db = SQLAlchemy()
 
 def get_db_connection():
     return mysql.connector.connect(
         host='localhost',
         user='root',
-        password='PONajith#2005',
+        password='PONajith#2005',  # Use your actual password
         database='uzhavan_db'
     )
