ec54dfd295a57d7469472892556cfd0ea70d278f
PON-AJITH-KUMAR-PONRAJ <ponajithkumar05@gmail.com>
2024-11-06 07:50:09 +0530

    Worked on notification management through email

---
+++ backend/config.py
@@ -0,0 +1,8 @@
+MAIL_SERVER = 'smtp.gmail.com'
+MAIL_PORT = 587
+MAIL_USE_TLS = True
+MAIL_USERNAME = 'your_email@gmail.com'
+MAIL_PASSWORD = 'your_email_password'
+
+SQLALCHEMY_DATABASE_URI = 'sqlite:///app.db'
+SQLALCHEMY_TRACK_MODIFICATIONS = False
