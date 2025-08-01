5c6e3faa38fda1fc1126c80456eb1e1a1e1d46a6
PON-AJITH-KUMAR-PONRAJ <ponajithkumar05@gmail.com>
2025-08-01 05:54:40 +0530

    Remove backend files from Git tracking (except models.py) and update .gitignore

--- backend/config.py
+++
@@ -1,7 +0,0 @@
-import os
-
-class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-    SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://root:PONajith#2005@localhost/uzhavu_db'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    JWT_SECRET_KEY = '5180db6812cb8827379272f2c7014b1b4fd90c4d533b2d95ee3ef43a062b0895'
